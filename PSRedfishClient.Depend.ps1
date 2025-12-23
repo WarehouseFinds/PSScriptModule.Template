e30d3603d915d890b3867004079b61c169ce8018
Marko Stanojevic <m.stanojevic@outlook.com>
2025-12-23 17:58:05 +0100

    Change worker node to Windows

--- PSRedfishClient.Depend.ps1
+++ PSRedfishClient.Depend.ps1
@@ -2,4 +2,5 @@
     # Build dependencies
     Pester           = @{ target = 'CurrentUser'; version = 'latest' }
     PSScriptAnalyzer = @{ target = 'CurrentUser'; version = 'latest' }
+    PlatyPS          = @{ target = 'CurrentUser'; version = 'latest' }
 }
