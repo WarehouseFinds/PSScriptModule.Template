769e1980551dd21871a0643ea9d7842adef6a7d3
Marko Stanojevic <m.stanojevic@outlook.com>
2025-12-24 21:59:58 +0100

    Listing more files

--- PSRedfishClient.build.ps1
+++ PSRedfishClient.build.ps1
@@ -121,7 +121,6 @@ task GenerateNewModuleVersion {
 
 # Update the module manifest with module version
 task UpdateModuleManifest GenerateNewModuleVersion, {
-    Get-ChildItem -Path $Script:moduleManifestPath
     Get-ChildItem -path $Script:moduleSourcePath
     $Params = @{
         Path          = $Script:moduleManifestPath
