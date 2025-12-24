e56870b05f579f1771e4f02b3ee6ff8f3cc1b552
Marko Stanojevic <m.stanojevic@outlook.com>
2025-12-24 19:56:58 +0100

    Resolve GitActions detections and update licence

--- PSRedfish.build.ps1
+++ PSRedfish.build.ps1
@@ -8,15 +8,18 @@
     This script contains the tasks for building the 'SampleModule' PowerShell module
 #>
 
+[CmdletBinding(DefaultParameterSetName = 'Debug')]
 Param (
-    [Parameter(ValueFromPipelineByPropertyName = $true)]
+    [Parameter(Mandatory = $true, ParameterSetName = 'Debug', ValueFromPipelineByPropertyName = $true)]
+    [Parameter(Mandatory = $true, ParameterSetName = 'Release', ValueFromPipelineByPropertyName = $true)]
     [ValidateSet('Debug', 'Release')]
     [String]
-    $Configuration = 'Debug',
-    [Parameter(ValueFromPipelineByPropertyName = $true)]
+    $ReleaseType = 'Debug',
+
+    [Parameter(Mandatory = $true, ParameterSetName = 'Release', ValueFromPipelineByPropertyName = $true)]
     [ValidateNotNullOrEmpty()]
     [String]
-    $SourceLocation
+    $SemanticVersion
 )
 
 Set-StrictMode -Version Latest
@@ -29,9 +32,6 @@ task . Clean, Build
 Enter-Build {
 
     # Installing PSDepend for dependency management
-    if (-not (Get-Module -Name PSDepend -ListAvailable)) {
-        Install-Module PSDepend -Force
-    }
     Import-Module PSDepend
 
     # Installing dependencies
@@ -80,8 +80,8 @@ task Analyze {
     Invoke-Pester -Configuration $Config
 }
 
-# Synopsis: Test the project with Pester tests
-task Test {
+# Synopsis: Run Pester tests Unit tests and generate code coverage report
+task UnitTest {
     #$files = Get-ChildItem -Path $moduleSourcePath -Recurse -Force -Include '*.ps1' -Exclude '*.Tests.ps1', '*build.ps1'
     
     $Config = New-PesterConfiguration @{
@@ -105,35 +105,18 @@ task Test {
     }
 
     # Invoke all tests
-    Invoke-Pester -Configuration $Config -Verbose
+    $result = Invoke-Pester -Configuration $Config -Verbose
+
+    # Fail the task if the code coverage results are not acceptable
+    if ( $result.CodeCoverage.CoveragePercent -lt $result.CodeCoverage.CoveragePercentTarget) {
+        throw "The overall code coverage by Pester tests is $("0:0.##" -f $result.CodeCoverage.CoveragePercent)% which is less than quality gate of $($result.CodeCoverage.CoveragePercentTarget)%. Pester ModuleVersion is: $((Get-Module -Name Pester -ListAvailable).ModuleVersion)."
+    }
 }
 
 # Synopsis: Generate a new module version if creating a release build
-task GenerateNewModuleVersion -If ($Configuration -eq 'Release') {
+task GenerateNewModuleVersion -If ($ReleaseType -eq 'Release') {
     # Using the current NuGet package version from the feed as a version base when building via Azure DevOps pipeline
 
-    # Define package repository name
-    $repositoryName = $moduleName + '-repository'
-
-    # Register a target PSRepository
-    try {
-        Register-PSRepository -Name $repositoryName -SourceLocation $SourceLocation -InstallationPolicy Trusted
-    }
-    catch {
-        throw "Cannot register '$repositoryName' repository with source location '$SourceLocation'!"
-    }
-
-    # Define variable for existing package
-    $existingPackage = $null
-
-    try {
-        # Look for the module package in the repository
-        $existingPackage = Find-Module -Name $moduleName -Repository $repositoryName
-    }
-    # In no existing module package was found, the base module version defined in the script will be used
-    catch {
-        Write-Warning "No existing package for '$moduleName' module was found in '$repositoryName' repository!"
-    }
 
     # If existing module package was found, try to install the module
     if ($existingPackage) {
@@ -145,30 +128,6 @@ task GenerateNewModuleVersion -If ($Configuration -eq 'Release') {
         [int]$Major = $currentModuleVersion.Major
         [int]$Minor = $currentModuleVersion.Minor
         [int]$Build = $currentModuleVersion.Build
-
-        try {
-            # Install the existing module from the repository
-            Install-Module -Name $moduleName -Repository $repositoryName -RequiredVersion $existingPackage.Version
-        }
-        catch {
-            throw "Cannot import module '$moduleName'!"
-        }
-
-        # Get the count of exported module functions
-        $existingFunctionsCount = (Get-Command -Module $moduleName | Where-Object -Property Version -EQ $existingPackage.Version | Measure-Object).Count
-        # Check if new public functions were added in the current build
-        [int]$sourceFunctionsCount = (Get-ChildItem -Path "$moduleSourcePath\Public\*.ps1" -Exclude "*.Tests.*" | Measure-Object).Count
-        [int]$newFunctionsCount = [System.Math]::Abs($sourceFunctionsCount - $existingFunctionsCount)
-
-        # Increase the minor number if any new public functions have been added
-        if ($newFunctionsCount -gt 0) {
-            [int]$Minor = $Minor + 1
-            [int]$Build = 0
-        }
-        # If not, just increase the build number
-        else {
-            [int]$Build = $Build + 1
-        }
 
         # Update the module version object
         $Script:newModuleVersion = New-Object -TypeName 'System.Version' -ArgumentList ($Major, $Minor, $Build)
@@ -205,7 +164,7 @@ task UpdatePackageSpecification GenerateNewModuleVersion, {
 #task Build UpdateModuleManifest, UpdatePackageSpecification, {
 task Build {
     # Warning on local builds
-    if ($Configuration -eq 'Debug') {
+    if ($ReleaseType -eq 'Debug') {
         Write-Warning "Creating a debug build. Use it for test purpose only!!!"
     }
 
@@ -220,49 +179,17 @@ task Build {
     Build-Module -Path $BuildRoot -OutputDirectory $buildOutputPath -ErrorAction Stop
 }
 
-# Synopsis: Verify the code coverage by tests
-task CodeCoverage {
-    $files = Get-ChildItem $moduleSourcePath -Dir -Force -Recurse |
-    Where-Object { $_.FullName -notLike '*build*' -and $_.FullName -notLike '*.git*' } |
-    Get-ChildItem -File -Force -Include '*.ps1' -Exclude '*.Tests.ps1' |
-    Select-Object -ExpandProperty FullName
-
-    $Config = New-PesterConfiguration @{
-        Run          = @{
-            Path     = $testSourcePath
-            PassThru = $true
-        }
-        Output       = @{
-            Verbosity = 'Normal'
-        }
-        CodeCoverage = @{
-            Enabled = $true
-            Path    = $files
-        }
-    }
-
-    # Additional parameters on Azure Pipelines agents to generate code coverage report
-    if ($env:TF_BUILD) {
-        if (-not (Test-Path -Path $buildOutputPath -ErrorAction SilentlyContinue)) {
-            New-Item -Path $buildOutputPath -ItemType Directory
-        }
-        $Timestamp = Get-date -UFormat "%Y%m%d-%H%M%S"
-        $PSVersion = $PSVersionTable.PSVersion.Major
-        $TestResultFile = "CodeCoverageResults_PS$PSVersion`_$TimeStamp.xml"
-        $Config.CodeCoverage.OutputPath = "$buildOutputPath\$TestResultFile"
-    }
-
-    $result = Invoke-Pester -Configuration $config
-
-    # Fail the task if the code coverage results are not acceptable
-    if ( $result.CodeCoverage.CoveragePercent -lt $result.CodeCoverage.CoveragePercentTarget) {
-        throw "The overall code coverage by Pester tests is $("0:0.##" -f $result.CodeCoverage.CoveragePercent)% which is less than quality gate of $($result.CodeCoverage.CoveragePercentTarget)%. Pester ModuleVersion is: $((Get-Module -Name Pester -ListAvailable).ModuleVersion)."
-    }
-}
-
 # Synopsis: Clean up the target build directory
 task Clean {
     if (Test-Path $buildOutputPath) {
-        Remove-Item -Path $buildOutputPath -Recurse
+        Write-Warning "Removing build output folder at '$buildOutputPath'"
+        $requestParam = @{
+            Path   = $buildOutputPath
+            Recurse = $true
+            Force   = $true
+        }
+        [void] (Remove-Item @requestParam)
     }
+
+    Write-Information "Build output folder cleaned up."
 }
