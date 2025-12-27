f4155a00add5bbc5bf9b1dcfe3889d3bd3d174ea
Marko Stanojevic <m.stanojevic@outlook.com>
2025-12-27 13:47:19 +0100

    Fix missing properties

--- src/Public/Get-PSScriptModuleInfo.ps1
+++ src/Public/Get-PSScriptModuleInfo.ps1
@@ -9,28 +9,11 @@ function Get-PSScriptModuleInfo {
     $moduleManifest = Import-PowerShellDataFile -Path $ModulePath 
     # Create a custom object to hold module information
     $moduleInfo = [PSCustomObject]@{
-        Name         = $moduleManifest.ModuleName
-        Version      = $moduleManifest.ModuleVersion
-        Author       = $moduleManifest.Author
-        Description  = $moduleManifest.Description
-        RootModule   = $moduleManifest.RootModule
-        Cmdlets      = @()
-        Functions    = @()
-        Variables    = @()
-        Aliases      = @()
-        Dependencies = $moduleManifest.RequiredModules
-    }
-
-    # Get exported cmdlets, functions, variables, and aliases
-    $exportedItems = Get-Module -Name $moduleInfo.Name -ListAvailable | Select-Object -ExpandProperty ExportedCommands
-
-    foreach ($item in $exportedItems) {
-        switch ($item.CommandType) {
-            'Cmdlet' { $moduleInfo.Cmdlets += $item.Name }
-            'Function' { $moduleInfo.Functions += $item.Name }
-            'Variable' { $moduleInfo.Variables += $item.Name }
-            'Alias' { $moduleInfo.Aliases += $item.Name }
-        }
+        ModuleVersion = $moduleManifest.Version
+        GUID          = $moduleManifest.GUID
+        Author        = $moduleManifest.Author
+        Description   = $moduleManifest.Description
+        Company       = $moduleManifest.Company
     }
 
     return $moduleInfo
